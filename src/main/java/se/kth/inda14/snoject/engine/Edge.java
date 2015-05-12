598b64caea4b85ceaf7f55151016079f561a0ab3
Mauritz <mauritzz@kth.se>
2015-05-12 20:31:31 +0200

    Documented and refactored Node and Edge classes

--- src/main/java/se/kth/inda14/snoject/engine/Edge.java
+++ src/main/java/se/kth/inda14/snoject/engine/Edge.java
@@ -1,18 +1,32 @@
 package se.kth.inda14.snoject.engine;
 
-// TODO: Documentation
+/**
+ * Represents a Graph edge (i.e. a link between two Nodes in the Graph)
+ *
+ * An Edge contains a starting and an ending point (Nodes, as well as
+ * properties for the Edge. Properties include what Provider provides
+ * the link between the two Nodes, and time, cost, and environmental data.
+ *
+ * Edges are immutable objects.
+ */
 public class Edge
 {
+    // Destination data
     private final Node from;
     private final Node to;
 
+    // Edge core data
     private final String name;
     private final Provider provider;
 
+    // Edge properties
     private final int cost;
     private final int time;
     private final int environment;
 
+    /**
+     * Construct a new Edge object with given properties.
+     */
     public Edge(Node from, Node to, String name, Provider provider, int cost, int time, int environment)
     {
         this.from = from;
@@ -24,52 +38,20 @@ public class Edge
         this.environment = environment;
     }
 
-    public Node getFrom()
-    {
-        return from;
-    }
-
-    public Node getTo()
-    {
-        return to;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public Provider getProvider()
-    {
-        return provider;
-    }
-
-    public int getCost()
-    {
-        return cost;
-    }
-
-    public int getTime()
-    {
-        return time;
-    }
-
-    public int getEnvironment()
-    {
-        return environment;
-    }
+    // Getters
+    public Node getFrom()           { return from; }
+    public Node getTo()             { return to; }
+    public String getName()         { return name; }
+    public Provider getProvider()   { return provider; }
+    public int getCost()            { return cost; }
+    public int getTime()            { return time; }
+    public int getEnvironment()     { return environment; }
 
     @Override
     public String toString()
     {
-        return "Edge{" +
-                "from=" + from +
-                ", to=" + to +
-                ", name='" + name + '\'' +
-                ", provider=" + provider +
-                ", cost=" + cost +
-                ", time=" + time +
-                ", environment=" + environment +
-                '}';
+        return String.format(
+                "Edge{from=%s, to=%s, name=%s, provider=%s, cost=%d, time=%d, environment=%d}",
+                from, to, name, provider, cost, time, environment);
     }
 }
--- src/main/java/se/kth/inda14/snoject/engine/Node.java
+++ src/main/java/se/kth/inda14/snoject/engine/Node.java
@@ -2,34 +2,29 @@ package se.kth.inda14.snoject.engine;
 
 /**
  * Represents a Graph Node (i.e. a destination in the graph)
+ *
+ * A Node contains a node ID, and a human readable name.
+ *
+ * Nodes are immutable objects.
  */
 public class Node
 {
     private final int id;
     private final String name;
 
+    /**
+     * Construct a new Node with given properties.
+     */
     public Node(int id, String name)
     {
         this.id = id;
         this.name = name;
     }
 
-    public int getId()
-    {
-        return id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
+    public int getId() { return id; }
+    public String getName() { return name; }
 
     @Override
     public String toString()
-    {
-        return "Node{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
+        { return String.format("Node{id=%d, name=%s}", id, name); }
 }
