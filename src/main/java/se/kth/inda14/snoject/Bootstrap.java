3de7124d1fb67cfe1e7b4126533658da79cecb3e
Mauritz <mauritzz@kth.se>
2015-05-12 20:57:44 +0200

    Documented DataSource, completed buildGraph method of Bootstrap

--- src/main/java/se/kth/inda14/snoject/Bootstrap.java
+++ src/main/java/se/kth/inda14/snoject/Bootstrap.java
@@ -2,13 +2,10 @@ package se.kth.inda14.snoject;
 
 import se.kth.inda14.snoject.datasources.XMLDataSource;
 import se.kth.inda14.snoject.engine.Edge;
-import se.kth.inda14.snoject.engine.Node;
-import se.kth.inda14.snoject.engine.Provider;
 import se.kth.inda14.snoject.graphs.HashGraph;
 import se.kth.inda14.snoject.interfaces.DataSource;
 import se.kth.inda14.snoject.interfaces.Graph;
 
-import java.util.Map;
 import java.util.Set;
 
 public class Bootstrap
@@ -45,9 +42,8 @@ public class Bootstrap
 
     public Bootstrap buildGraph()
     {
-        Map<Integer, Node> nodes = ds.getNodes();
         Set<Edge> edges = ds.getEdges();
-        Map<Integer, Provider> providers = ds.getProviders();
+        edges.forEach(g::add);
 
         return this;
     }
--- src/main/java/se/kth/inda14/snoject/interfaces/DataSource.java
+++ src/main/java/se/kth/inda14/snoject/interfaces/DataSource.java
@@ -9,15 +9,45 @@ import java.util.Set;
 
 /**
  * Represents an arbitrary Graph data source.
+ *
+ * Data sources first connect, load or initialize using the init()
+ * method. Nodes, Edges and Providers can then be accessed.
+ *
+ * Method return values must comply with Edge, Node and Provider classes.
  */
-// TODO: Documentation
 public interface DataSource
 {
+    /**
+     * Init the Data Source by loading and connecting to necessary resources.
+     *
+     * @throws          Exception upon initialization error encounter
+     */
     void init() throws Exception;
 
+    /**
+     * Get all Node objects by loading them from the data source.
+     * Should not return any malformed Node objects.
+     *
+     * @return          Map with a Node's ID as key and its object as value
+     */
     Map<Integer, Node> getNodes();
 
+    /**
+     * Get all Edge objects by loading them from the data source.
+     * Should not return any malformed Edge objects.
+     *
+     * Construction of Edge objects is dependant on Nodes and Providers,
+     * since an Edge object contains references to both.
+     *
+     * @return          A Set of all Edge objects
+     */
     Set<Edge> getEdges();
 
+    /**
+     * Get all Provider objects by loading them from the data source.
+     * Should not return any malformed Provider objects.
+     *
+     * @return          Map with a Provider's ID as key and its object as value
+     */
     Map<Integer, Provider> getProviders();
 }
