13372650b9320f3fecce8319cdc47febf7d70775
mauritz <mauritzz@kth.se>
2015-05-17 21:04:15 +0200

    Added NodeAction, implemented first un-tested version of BFS

--- src/main/java/se/kth/inda14/snoject/engine/GraphSearch.java
+++ src/main/java/se/kth/inda14/snoject/engine/GraphSearch.java
@@ -1,7 +1,9 @@
 package se.kth.inda14.snoject.engine;
 
-import java.util.Map;
-import java.util.Set;
+import se.kth.inda14.snoject.interfaces.Graph;
+import se.kth.inda14.snoject.interfaces.NodeAction;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class GraphSearch
@@ -14,5 +16,59 @@ public class GraphSearch
 				.filter(n -> n.getName() != null)
 				.filter(n -> n.getName().toLowerCase().startsWith(startsWith.toLowerCase()))
 				.collect(Collectors.toSet());
+	}
+
+	/**
+	 * Search through a Graph object using Breadth-First Search.
+	 *
+	 * @param g			Graph object to search
+	 * @param v			Vertex to begin search from
+	 * @param action	Visit action to perform
+	 */
+	public void bfs(Graph g, Node v, NodeAction action)
+	{
+		// Use a Queue for BFS
+		Queue<GraphPath> q = new LinkedList<>();
+		Map<Node, Boolean> visited = new HashMap<>();
+
+		visited.put(v, true);
+		q.add(new GraphPath(v, v));
+
+		while(!q.isEmpty())
+		{
+			// De-queue a GraphPath and perform visit action
+			GraphPath n = q.remove();
+			action.act(g, n.current, n.previous);
+
+			Iterator<Node> it = g.neighbors(n.current);
+
+			while (it.hasNext())
+			{
+				Node i = it.next();
+
+				// If the Vertex is not visited, visit and add it to queue
+				if (!visited.get(i))
+				{
+					visited.put(i, true);
+					q.add(new GraphPath(i, n.current));
+				}
+			}
+		}
+	}
+
+
+}
+
+/**
+ * Package local class to keep track of a vertex, and a previously visited vertex
+ */
+class GraphPath
+{
+	public Node current, previous;
+
+	public GraphPath(Node current, Node previous)
+	{
+		this.current = current;
+		this.previous = previous;
 	}
 }
---
+++ src/main/java/se/kth/inda14/snoject/interfaces/NodeAction.java
@@ -0,0 +1,23 @@
+package se.kth.inda14.snoject.interfaces;
+
+import se.kth.inda14.snoject.engine.Node;
+import se.kth.inda14.snoject.interfaces.Graph;
+
+/**
+ * This interface contains a single act method intended to be called during a
+ * traversal of a graph.
+ *
+ * @author Stefan Nilsson
+ * @version 2012-12-30
+ */
+public interface NodeAction
+{
+	/**
+	 * An action performed when a search of the graph g visits node current.
+	 *
+	 * @param g			a graph
+	 * @param current	current Node in the graph
+	 * @param previous	previous Node in the graph
+	 */
+	void act(Graph g, Node current, Node previous);
+}
